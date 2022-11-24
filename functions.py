243235fea08f6c28ae2c155aa1c9639699d0976f
Bujáki-Korsós Franciska <bujakikorsos.franciska@studemts.jedlik.eu>
2022-11-24 12:25:40 +0100

    menü

--- functions.py
+++ functions.py
@@ -2,3 +2,16 @@ from data import bevetelekOsszeg, bevetelMegnevezes, kiadadOsszeg, kiadasMegneve
 from os import system
 kiadas='kiadasok.txt'
 bevetel='bevetelek.txt'
+
+def menu():
+    system('cls')
+    print('MENÜ:')
+    print('\t 0 - Kilépés')
+    print('\t 1 - Kiadások')
+    print('\t 2 - Bevételek')
+    print('\t 3 - Új kiadás felvétele')
+    print('\t 4 - Új bevétel felvétele')
+    print('\t 5 - Kiadás törlése')
+    print('\t 6 - Bevétel törlése')
+    print('\t 7 - Kiadások összesen')
+    print('\t 8 - Bevételek összesen')
