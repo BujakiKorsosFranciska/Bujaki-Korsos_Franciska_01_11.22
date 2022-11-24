61af3290a3c6be234e3c441e19dc7263e0d5a2d5
Bujáki-Korsós Franciska <bujakikorsos.franciska@studemts.jedlik.eu>
2022-11-24 12:36:31 +0100

    main

---
+++ main.py
@@ -0,0 +1,23 @@
+from functions import *
+from os import system
+
+valasztas=''
+while valasztas!='0':
+    valasztas=menu()
+    if valasztas=='1':
+        pass
+    elif valasztas=='2':
+        pass
+    elif valasztas=='3':
+        pass
+    elif valasztas=='4':
+        pass
+    elif valasztas=='5':
+        pass
+    elif valasztas=='6':
+        pass
+    elif valasztas=='7':
+        pass
+    elif valasztas=='8':
+        pass
+    
